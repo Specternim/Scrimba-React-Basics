b74a2da16990a6c152941ba6f0bdb74817e525a5
Vishal Alkari <vishalalkari999@gmail.com>
2023-03-14 23:44:08 +0530

    initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="../public/atom.png" alt="react logo" className="logo" />
+      <h3>ReactFacts</h3>
+      <h4>React Project - 1</h4>
+    </nav>
+  );
+}
