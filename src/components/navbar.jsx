8c0ca6aad2eeb1bdb350120bf40402a8ffc8af30
Vishal Alkari <vishalalkari999@gmail.com>
2023-03-14 23:50:31 +0530

    attempt-1: fix logo not displayed on site

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav>
-      <img src="../public/atom.png" alt="react logo" className="logo" />
+      <img src="../public/atom.png" alt="atom logo" className="logo" />
       <h3>ReactFacts</h3>
       <h4>React Project - 1</h4>
     </nav>
